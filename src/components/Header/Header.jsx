52700f789eaeb2cfcf74fcafaf641e33ff41633b
Arseny <iv.ars@mail.ru>
2021-06-06 00:28:03 +0300

    Social_network
    Social_network

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
+
+const Header = (props) => {
+    return <header className={s.header}>
+        <img src='https://purepng.com/public/uploads/thumbnail//google-stadia-logo-hd4.png' />
+
+        <div className={s.loginBlock}>
+            { props.isAuth
+                ? <div>{props.login} - <button onClick={props.logout}>Log out</button> </div>
+                : <NavLink to={'/login'}>Login</NavLink> }
+        </div>
+    </header>
+}
+
+export default Header;
